e900a84608bdaf0f4606a00334ea5971e2a54e12
MohammedAbdullahA <151395935+MohammedAbdullahA@users.noreply.github.com>
2024-07-27 20:43:14 +0300

    Update step2b.py

--- step2b.py
+++ step2b.py
@@ -1,1 +1,2 @@
+# There are inconsistent values and it will be fixed in the coming code
 print(f'The number of null rows for each column are:\n{df.isna().sum()}') #number of rows that contain null values
