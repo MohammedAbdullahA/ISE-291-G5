930af2abeb684f9b43fddcb30084e2f29498b3ae
MohammedAbdullahA <151395935+MohammedAbdullahA@users.noreply.github.com>
2024-07-27 21:37:28 +0300

    Update step2b.py

--- step2b.py
+++ step2b.py
@@ -1,2 +1,11 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 # There are inconsistent values and it will be fixed in the coming code
-print(f'The number of null rows for each column are:\n{df.isna().sum()}') #number of rows that contain null values
+print(f'The number of null rows for each column are:\n{df.isna().sum()}') #number of rows that contain null values (incomplete)
+# Identify numeric columns
+num_columns = df.select_dtypes(exclude='object').columns #identifying numerical columns
+
+for c in num_columns: #drawing box plots to identify and list outliers
+    plt.figure()
+    sns.boxplot(y=c,x='BMI Category',data=df);
+    plt.show()
